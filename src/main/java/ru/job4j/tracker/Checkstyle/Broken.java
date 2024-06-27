aa78c36f642082c7604964d4af74c54c6c9d269f
Дмитрий <delasko@mail.ru>
2024-06-27 21:54:19 +0400

    2. Репозиторий под проект Tracker [#245623]

---
+++ src/main/java/ru/job4j/tracker/Checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.Checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    private static final String NEW_VALUE = "";
+    private String surname;
+    private String name;
+
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d) {
+
+    }
+}
